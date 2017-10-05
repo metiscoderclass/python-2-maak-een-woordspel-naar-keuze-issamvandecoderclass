ebe5ed9d2de5ea2239e5518bcf8dd541b670d100
issam <issam.buamran@coderclass.nl>
2017-10-05 09:44:12 +0200

    test

---
+++ hangman.py
@@ -0,0 +1,41 @@
+def een():
+    print("=========")
+
+
+
+
+def twee():
+    print("=========")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+
+def drie():
+    print("=========")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("=========")
+
+def vier():
+    print("=========")
+    print("|       | ")
+    print("|       ")
+    print("|")
+    print("|")
+    print("=========")
+
+def vijf():
+    print("=========")
+    print("|       | ")
+    print("|      ( )")
+    print("|      \|/ ")
+    print("|       | ")
+    print("|      / /")
+    print("|")
+    print("=========")
---
+++ python.py
@@ -0,0 +1,45 @@
+import hangman
+import random
+
+woorden = ["informatica", "linux", "python", "windows", ""]
+counter = 0
+foute_letter = ""
+
+
+
+def check():
+    if counter == 1:
+        hangman.een()
+    elif counter == 2:
+        hangman.twee()
+    elif counter == 3:
+        hangman.drie()
+    elif counter == 4:
+        hangman.vier()
+    elif counter == 5:
+        hangman.vijf()
+
+def inwoord():
+
+
+woord=random.choice(woorden)
+
+print(woord)
+
+letter = input("type een letter ")
+
+
+
+while True:
+    if letter in woord:
+        print("Het zit in de geheime woord.")
+    elif letter == "?":
+        raad=input("Type de woord dan ")
+        if raad == woord:
+            print("Ooh je hebt het woord geraden ")
+            break
+        else:
+            print("je hebt het woord fout geraden ")
+            counter += 1
+            check()
+
